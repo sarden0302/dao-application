24b91e53fa418722ed39aa6bdb4e96b3c6fbca86
sarden0302 <sarden030@gmail.com>
2025-01-21 13:21:49 +0900

    JPA DAO PRODUCT GET(FINDALL) POST(SAVE)

--- src/main/java/edu/kh/com/daoapplication/repository/KHTProductRepository.java
+++ src/main/java/edu/kh/com/daoapplication/repository/KHTProductRepository.java
@@ -2,6 +2,8 @@ package edu.kh.com.daoapplication.repository;
 
 import edu.kh.com.daoapplication.dao.KHTProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface KHTProductRepository extends JpaRepository<KHTProduct, Long> {
 }
